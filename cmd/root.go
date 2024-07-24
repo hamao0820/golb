cd49dc18d677a2e337881c7a3f5ee429d142992f
浜田　俊祐 <hamashun0820@icloud.com>
2024-07-24 14:46:30 +0900

    flagを追加

--- cmd/root.go
+++ cmd/root.go
@@ -10,15 +10,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var libPackage string
+var rootDir string
+
 var rootCmd = &cobra.Command{
 	Use:   "golb",
 	Short: "",
 	Long:  "",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		b := golb.NewBundler("golb/testdata/src/a/main.go", "github.com/hamao0820/ac-library-go", "golb/testdata")
+		b := golb.NewBundler("golb/testdata/src/a/main.go", libPackage, rootDir)
 		err := b.Bundle()
 		return err
 	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&libPackage, "lib", "l", "github.com/hamao0820/ac-library-go", "target library package")
+	rootCmd.PersistentFlags().StringVarP(&rootDir, "root", "r", "golb/testdata", "root directory")
 }
 
 func Execute() {
