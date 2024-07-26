763b7ea14364f41834e281480e941017732fa3f1
浜田　俊祐 <hamashun0820@icloud.com>
2024-07-26 10:05:24 +0900

    テストケースを追加

--- golb/testdata/src/a/main.go
+++ golb/testdata/src/a/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hamao0820/ac-library-go/lib/sample"
 	"github.com/hamao0820/ac-library-go/lib/sample/nested"
+	segmenttree "github.com/hamao0820/ac-library-go/lib/segment_tree"
 	vc "github.com/hamao0820/ac-library-go/lib/vector"
 )
 
@@ -13,6 +14,9 @@ func main() {
 	v2 := vc.NewVector(3, 4)
 	v3 := v1.Add(v2) // v3 = v1 + v2
 	// v3.Scale(2)
+
+	st := segmenttree.NewSegmentTree(10, 0, func(a, b int) int { return a + b })
+	st.Update(0, 1)
 
 	fmt.Println(v3.X, v3.Y)
 	fmt.Println(sample.Sample(1, 2))
